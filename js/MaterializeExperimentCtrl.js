892b79efd0dd9dc54a1b7837368425f6552e457e
Peps87 <giuseppevale@gmail.com>
2017-04-11 20:37:26 +0200

    First stable photoGallery
    Prima versione photogallery stable

--- js/MaterializeExperimentCtrl.js
+++ js/MaterializeExperimentCtrl.js
@@ -1,5 +1,5 @@
 angular.module('myApp', [])
-    .controller('MaterializeExperimentCtrl', function($scope, $http) {
+    .controller('MaterializeExperimentCtrl', function($scope, $http, $timeout) {
         var vm = this;
         (function($) {
             $(function() {
@@ -13,8 +13,6 @@ angular.module('myApp', [])
                 $(window).scroll(function() {
                     positionAreaVisible = $(window).scrollTop();
                     areaVisible = window.innerHeight;
-                    console.log(positionAreaVisible);
-
                 });
 
 
@@ -24,32 +22,25 @@ angular.module('myApp', [])
                         var fotoElementCnt = $("#container_" + fotoIdOld);
                         var fotoElementX = $("#" + fotoIdOld);
                         var fotoElementY = $("#interno_" + fotoIdOld);
-
-                        //fotoElementX.removeClass("active");
                         var initialPositionSTATIC = fotoElementStatic.offset();
                         var initialPositionECX = fotoElementCnt.offset();
                         var lastPositionX = fotoElementX.offset();
                         var lastPositionY = fotoElementY.offset();
-                        //   var calcXtranslateForClose = initialPositionECX.left - lastPositionX.left;
-                        //   var calcYtranslateForClose = initialPositionECX.top - lastPositionY.top;
                         var calcYtranslateForSTATIC = initialPositionSTATIC.top - initialPositionECX.top;
                         var translateStaticPosition = "translateY(" + calcYtranslateForSTATIC + "px)";
-                        //  translateOldXposition = "translateX(" + calcXtranslateForClose + "px)";
-                        // translateOldYposition = "translateY(" + calcYtranslateForClose + "px)" + " scale(1)";
                         translateOldXposition = "translateX(0px)";
                         translateOldYposition = "translateY(0px)" + " scale(1)";
-                        fotoElementX.css("transform", translateOldXposition, "z-index", "0");
                         var inner = initialPositionSTATIC.top - positionAreaVisible;
-
                         fotoElementCnt.css({ "top": inner, "position": "fixed" });
-                        //    fotoElementCnt.css("transform", translateStaticPosition, "z-index", "0");
+                        fotoElementX.css("transform", translateOldXposition, "z-index", "0");
                         fotoElementY.css("transform", translateOldYposition, "z-index", "0");
                         fotoElementX.css("transform", "none");
                         fotoElementY.css("transform", "none");
-                        // fotoElementCnt.removeClass("active");
-                        fotoElementCnt.removeAttr("style");
-                        //  fotoElementCnt.css("transform", "none");
-                        //fotoIdOld = "";
+                        $timeout(function() {
+                            fotoElementCnt.removeAttr("style");
+                        }, 500);
+
+
                     }
 
                 }
@@ -73,24 +64,31 @@ angular.module('myApp', [])
                         var ElementCONTAINER = $("#container_" + fotoId);
                         var ElementContainerX = $("#" + fotoId);
                         var ElementContainerY = $("#interno_" + fotoId);
+
+                        var hY = ElementContainerY.innerHeight();
+                        var lY = ElementContainerY.innerWidth();
+                        console.log(lY);
+                        console.log(hY);
+                        var l = lY / 2;
+                        var h = hY / 2;
+                        console.log(l);
+                        console.log(h);
                         var initialPositionECX = ElementCONTAINER.offset();
                         var widthWindow = $(window).width();
                         var lCenterAreaVisible = (widthWindow / 2) - 17;
-                        var hCenterAreaVisible = (areaVisible / 2) + positionAreaVisible;
+                        var hCenterAreaVisible = ((areaVisible / 2) + positionAreaVisible) - h;
                         var calcXtranslate = lCenterAreaVisible - initialPositionECX.left;
                         var calcYtranslate = hCenterAreaVisible - initialPositionECX.top;
                         var cssXtranslate = "translateX(" + calcXtranslate + "px)";
                         var cssYtranslate = "translateY(" + calcYtranslate + "px)" + " scale(3.1)";
+
                         ElementContainerX.css("transform", cssXtranslate, "z-index", "1");
                         ElementContainerY.css("transform", cssYtranslate, "z-index", "1");
 
                         var inner = initialPositionECX.top - positionAreaVisible;
-                        console.log("offset", initialPositionECX.top);
-                        console.log("inner", inner);
                         ElementCONTAINER.css({ "top": inner, "position": "fixed" });
-                        // ElementCONTAINER.addClass("active");
 
-                        //  ElementContainerX.addClass("active");
+
 
 
 
